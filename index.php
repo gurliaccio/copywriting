9754180726b9b66685530994ed2bf856a9928e44
Michael <gurliaccio@gmail.com>
2018-01-19 19:50:53 -0500

    shitten

--- index.php
+++ index.php
@@ -547,9 +547,11 @@
     <!-- FOOTER END -->
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) 
+        <script src="js/jquery.min.js">
+    -->
 
-    <script src="http://code.jquery.com/jquery-latest.min.js"></script> -->
-    <script src="js/jquery.min.js">
+    <script src="http://code.jquery.com/jquery-latest.min.js"></script> 
+
 
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>

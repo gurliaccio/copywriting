129b3ad146d03c3c81e41bbafea4d9f76b379a79
Michael <gurliaccio@gmail.com>
2018-01-19 19:41:51 -0500

    revert

--- index.php
+++ index.php
@@ -547,7 +547,9 @@
     <!-- FOOTER END -->
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="js/jquery-3.2.1.min.js"></script>
+    
+    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 

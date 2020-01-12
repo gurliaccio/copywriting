6cca2664f1757216422f09453dc1907212371fe9
Michael <gurliaccio@gmail.com>
2020-01-11 20:39:23 -0500

    Update index.php

--- index.php
+++ index.php
@@ -123,7 +123,7 @@
 
         <div class="row">
             <div class="col-lg-12 col-centered">
-                <h2 class="wow fadeInDown" data-wow-duration="2s">I like talking about technology, content strategy, brand development, UX writing, stand-up comedy, my drum set, Sardegna, exotic food...</h2>
+                <h3 class="wow fadeInDown" data-wow-duration="2s">I like talking about technology, content strategy, brand development, UX writing, stand-up comedy, my drum set, Sardegna, exotic food...</h3>
                 <br>
                 <p class="wow fadeInDown" data-wow-duration="3s">I’ve crafted original ideas, compelling copy, and engaging content for ad agencies and in-house marketing teams across the country.</p>
 

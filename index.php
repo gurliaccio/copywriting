63fb1925ac03a620d38626254e2239a5013f380e
Michael <gurliaccio@gmail.com>
2020-01-31 18:42:33 -0500

    Update index.php

--- index.php
+++ index.php
@@ -136,9 +136,6 @@
                 <!-- ABOUT INFO END -->
                 <br>
                 <!-- <div class="myapps row"> -->
-                
-                <h5 class="wow animated bounceInUp" data-wow-duration="7s">
-                    <a type="button" class="btn btn-info btn-lg wow animated bounceInUp" data-wow-duration="4s" href="https://www.linkedin.com/in/firebad/" target="_blank">Connect on LinkedIn</a>
 
                     <!-- <a href="https://twitter.com/atlcopywriter?ref_src=twsrc%5Etfw" class="btn btn-info btn-lg wow animated bounceInUp" data-show-count="false">Follow @atlcopywriter</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> -->
 
@@ -442,7 +439,9 @@
     <div id="testimonials" class="container">
         <div class="sectionhead wow bounceInUp" data-wow-duration="2s">
             <span class="bigicon icon-bubbles"></span>
-            <h3>Let's Talk</h3>
+            <!--  <h3>Let's Talk</h3> -->
+             <h5 class="wow animated bounceInUp" data-wow-duration="7s">
+                <a type="button" class="btn btn-info btn-lg wow animated bounceInUp" data-wow-duration="4s" href="https://www.linkedin.com/in/firebad/" target="_blank">Let's Talk</a>
             <h4>I'm currently available for freelance and full-time assignments.</h4>
             <h4>Drop me a line or give me a call anytime.</h4> 
             <h4>(404) 457-1496</h4> 
@@ -537,24 +536,6 @@
                     
                 </ul>
             </div>
-            <!-- FOOTER LINKS END -->
-
-            <!-- <div class="copyright">
-                
-                <p><a href="#">Drifolio</a> stands for Dribbble-Portolio. This is a free HTML template designed and coded by <a href="http://creativealiens.com">Creative Aliens</a>. All logo and images in this template used for preview purpose only. You are free to use the template but make sure to use your own images.</p>
-            </div> -->
-            <!-- FOOTER COPYRIGHT END -->
-
-           <!--  <div class="footersocial wow fadeInUp" data-wow-duration="3s">
-                <ul>
-                    <li><a href="http://facebook.com/MamunSrizon"><span class="icon-social-linkedin"></span></a></li>
-                    <li><a href="http://twitter.com/MamunSrizon"><span class="icon-social-twitter"></span></a></li>
-                    <li><a href="#"><span class="icon-social-youtube"></span></a></li>
-                    <li><a href="http://dribbble.com/srizon"><span class="icon-social-dribbble"></span></a></li>
-                    <li><a href="#"><span class="icon-social-tumblr"></span></a></li>
-                </ul>
-            </div> -->
-            <!-- FOOTER SOCIAL ICONS END -->
         </div>
     </footer>
     <!-- FOOTER END -->

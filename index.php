19f59836ac6ef3673b3b603a2039743d41d1d3a9
Michael <gurliaccio@gmail.com>
2022-07-28 18:16:31 -0400

    aec

--- index.php
+++ index.php
@@ -121,15 +121,15 @@
                             <h4 class="modal-title">Google Cloud</h4>
                         </div>
                         <div class="modal-body">
-                            <p>I really enjoy complex challenges, and this was a fun one.<br>Below is a live document I created on my first day 
+                            <p>I really enjoy complex challenges and this was a fun one.<br>Below is a live document I created on my first day 
                             and shared with the rest of my team. 2 other people! After collaborating with folks from acrosss Google Cloud and the globe, 
-                            we handed off a scalable content strategy focusing on creating a phased approach based on sales cycles.<br>
+                            we handed off a scalable content strategy that focused on a phased approach.<br>
                             Click the image to open our deck:</p>
                             <a href="https://docs.google.com/presentation/d/1LNPTraSroI-m43Q1N_hEM34ezUl9YLlTGL-ElqAP754/edit?resourcekey=0-wxAie2SqHt7xkzRdXPE02w">
                             <img style="border:1px solid black" src="img/CSBOM.jpeg"/>
                             </a>
                             <br>
-                            <p>Here's a glimpse of our new content lifecycle. There wasn't one before! We carefully, and with a ton of help from our teammates, established a lifecycle to quickly and easily create or update important marketing assets.</p>
+                            <p>Here's a glimpse of our new content lifecycle. There wasn't one before! With a ton of help from our teammates, we established a lifecycle to quickly and easily create or update important marketing assets.</p>
                             <img src="img/SEContentLifecycle.png">
                             <br>
                             <a href="https://www.youtube.com/watch?v=pPzPnQITGbY"><img src="img/13.jpeg"></a>
